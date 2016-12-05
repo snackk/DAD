9d9d8d4d522194dba209f9080b527b173f16476e
Literallywho <mastahiro@hotmail.com>
2016-12-05 14:05:03 +0000

    Added configuration file initial support (again again)

---
+++ DADSTORM/DataTypes/DataTypes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DADSTORM.DataTypes
+{
+    enum RoutingType
+    {
+        hashing,
+        random,
+        primary
+    }
+    enum OperatorType
+    {
+        dup,
+        filter,
+        custom,
+        uniq,
+        count
+    }
+
+    class ConfigurationData
+    {
+        public string NodeName { get; set; }
+        public string TargetData { get; set; }
+
+        public int NumberofReplicas { get; set; }
+        public RoutingType Routing { get; set; }
+        public int RoutingArg { get; set; }
+
+        public List<string> Addresses { get; set; }
+
+        public OperatorType Operation { get; set; }
+        public List<string> OperationArgs { get; set; }
+    }
+}
